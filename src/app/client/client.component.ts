392c6f9c908fdb649aeb62080d6dd252440cac58
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-01-02 09:25:05 +0600

    display the Add New Modal Popup Form

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 export class Client{
   constructor(
@@ -20,9 +21,11 @@ export class Client{
 export class ClientComponent implements OnInit {
 
   clients!: Client[];
+  closeResult!: string;
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit(): void {
@@ -36,6 +39,24 @@ export class ClientComponent implements OnInit {
         this.clients = response;
       }
     );
+  }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
